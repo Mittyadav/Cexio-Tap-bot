b71b78d97be474c577092f94b0d723b8df77db77
Mittyadav <146659530+Mittyadav@users.noreply.github.com>
2024-11-04 07:28:05 +0530

    Update launcher.py

--- bot/utils/launcher.py
+++ bot/utils/launcher.py
@@ -20,7 +20,7 @@ start_text = """
 /  ` |__  \_/ | /  \    |__) /  \  |  
 \__, |___ / \ | \__/    |__) \__/  |  
 
-            BY VANHBAKA                                                                                                       
+            BY DARK LIFE                                                                                                       
 
 Select an action:
 
